f966494eff4d92ce301d423b16cdb2121c297331
Vivek18v <112846370+Vivek18v@users.noreply.github.com>
2022-09-06 13:46:42 +0530

    Add files via upload

---
+++ factorial of given number.py
@@ -0,0 +1,9 @@
+
+#author:v.vivek
+#program name:factorial of a number
+#date:06/09/2022
+num=int (input("enter the number:"))
+factorial=1
+for i in range(1,num+1):
+    factorial=factorial*i
+print("factorial of ",factorial)
